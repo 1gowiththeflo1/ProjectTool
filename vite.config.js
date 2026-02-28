eb8e145f34c3676d47880a6057667c362662ff26
floriansteger <85990596+1gowiththeflo1@users.noreply.github.com>
2026-02-28 15:26:52 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+server: {
+  allowedHosts: ['devserver-main--porjectcalc.netlify.app'],
+}
+server: {
+  allowedHosts: true
+}
+
 export default defineConfig({
   plugins: [react()],
   build: {
